41ea71a81adbc39cd04ee4d96b84c72ec95e30ab
jaiakash2005 <jaiakashsubramaniyan@gmail.com>
2024-12-13 22:38:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function bookNow() {
+    alert('Booking feature coming soon!');
+}
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting us!');
+});
